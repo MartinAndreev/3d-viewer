81fce2bd0b3dc1b34f25250d849ed084ae327cba
Marto Andreev <marto@www-you.com>
2020-10-01 13:21:42 +0300

    Added the demo build

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
-const { resolve, join } = require('path');
+const { resolve, join } = require("path");
 
-const { CheckerPlugin } = require('awesome-typescript-loader');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const { CheckerPlugin } = require("awesome-typescript-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
 
   devServer: {
-    contentBase: join(__dirname, 'public'),
+    contentBase: join(__dirname, "public"),
   },
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: resolve(__dirname, "dist"),
   },
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
 
-  devtool: 'source-map',
+  devtool: "source-map",
 
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: ['awesome-typescript-loader'],
+        use: ["awesome-typescript-loader"],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-          'file-loader?hash=sha512&digest=hex&name=images/[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+          "file-loader?hash=sha512&digest=hex&name=images/[hash].[ext]",
+          "image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false",
         ],
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   plugins: [
     new CheckerPlugin(),
     new ProgressBarPlugin(),
     new HtmlWebpackPlugin({
       template: `${__dirname}/src/index.html`,
-      filename: 'index.html',
-      inject: 'body',
+      filename: "index.html",
+      inject: "body",
     }),
   ],
 };
